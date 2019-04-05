8c09aa859107379b6d81b0ee95767c54fb5f1d04
Sherief Farid <sherief@farid.cloud>
2019-04-05 22:52:42 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,15 +1,20 @@
 class Anagram
 
-  attr_accessor :word
+  attr_accessor :word, :word_array
 
 
   def initialize(word)
     @word = word
+    @word_array = @word.split(//)
   end
 
+  
 
-  def match
 
+  def match(array)
+    array.each do |c_word|
+      
+    end
   end
 
 
