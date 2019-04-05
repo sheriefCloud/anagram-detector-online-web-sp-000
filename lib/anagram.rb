8fa1f2966bb798b55d0dc5c5965b2aa7b587bbd9
Sherief Farid <sherief@farid.cloud>
2019-04-05 22:48:56 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+
+  def initialize(word)
+    @word = word
+  end
+
+
+  def match
+    
+  end
+
+
+
+
+
+end# Your code goes here!
